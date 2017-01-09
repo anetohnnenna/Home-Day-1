0174122c1a4d23163136e64dced6d48dac42d91f
Bolaji <cooproto@gmail.com>
2017-01-09 16:54:28 +0100

    Get Primes function to get list of prime numbers from 0 to a specified integer

--- getPrimes/app/library.js
+++ getPrimes/app/library.js
@@ -1,14 +1,41 @@
 'use strict'
 
-module.exports = {
- 
-  getPrimes: function(n) {
-    if ((n === 1) || (n === 0)) {
-      return "1 or 0 is not a prime number.";
-    }
-    else {
-      
-    }
+
+
+function getPrimes(num) {
+  var primes = [2,3];
+
+  var checkPrime = function(num) {
+    var numIsPrime;
+    for(var i = 2; i < num; i++){
+           if(num % i === 0){
+               numIsPrime = false ;
+               break;
+           }
+           else {
+            numIsPrime = true;
+           }
+      }
+      return numIsPrime;
   }
 
+  if (num < 0) {
+    return "negative integers can not be prime";
+  }
+  else if (num === 1) {
+    return [];  
+  }
+  else if ((num === 2) || (num === 3)) {
+    return primes;
+  }
+  else {
+    for(var i = 4; i <= num; i++) {
+          if(checkPrime(i) === true ){
+              primes.push(i);
+          }
+    }
+    return primes;
+  }
 }
+
+module.exports = getPrimes;
